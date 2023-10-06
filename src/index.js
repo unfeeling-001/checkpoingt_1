b38fa4be6273e53ef6f5ea8b8c05889fff65c12e
unfeeling-001 <hyanerkaev@gmail.com>
2023-10-06 10:52:53 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
